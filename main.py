90e2b4da701db430fed653d1c455e8abd261aab1
Jonathan Harooni <42428480+Speedy9d2@users.noreply.github.com>
2023-12-12 19:53:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_data = {row["letter"]: row["code"] for (index, row) in data.iterrows()}
 print(new_data)
 
-user_input = input("Enter a word ")
-result = [new_data[word.upper()] for word in user_input]
-print(result)
+
+def generated_answer():
+    user_input = input("Enter a word ")
+    try:
+        result = [new_data[word.upper()] for word in user_input]
+        print(result)
+    except KeyError:
+        print("Numbers are not allowed")
+        generated_answer()
+
+
+generated_answer()
